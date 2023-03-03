fc0e0e1bffec8d8988efff06085a4b0f833c75ab
Mohammad Assaf10 <mhamad234assaf@gmail.com>
2023-03-03 15:30:16 +0200

    Fix some code

--- lib/features/home/presentation/pages/main/main_view.dart
+++ lib/features/home/presentation/pages/main/main_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../config/app_localizations.dart';
 import '../../../../../config/assets_manager.dart';
+import '../../../../../config/color_manager.dart';
 import '../../../../../config/strings_manager.dart';
 import '../../../../../config/values_manager.dart';
 import '../../../../auth/presentation/bloc/authentication_bloc.dart';
@@ -21,81 +22,86 @@ class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     PageController controller = PageController(initialPage: 2);
-    return Scaffold(
-      body: PageView(
-        onPageChanged: (v) {
-          final authState = BlocProvider.of<AuthenticationBloc>(context).state;
-
-          if (v == 4 && authState is AuthenticationSuccess) {
-            BlocProvider.of<EmployeeBloc>(context)
-                .add(GetEmployeeInfo(id: authState.employee.employeeID));
-          }
-        },
-        controller: controller,
-        children: const [
-          OrdersScreen(),
-          MyWalletScreen(),
-          MyOrdersScreen(),
-          CustomersServiceScreen(),
-          ProfileScreen(),
-        ],
-      ),
-      floatingActionButton: MaterialButton(
-        onPressed: () => controller.jumpToPage(2),
-        child: Image.asset(
-          IconAssets.home,
-          filterQuality: FilterQuality.high,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        child: SizedBox(
-          height: AppSize.s50.h,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  NavigationBarBottom(
-                    title: AppStrings.orders.tr(context),
-                    icon: IconAssets.orders,
-                    onPress: () {
-                      // final state = BlocProvider.of<AuthenticationBloc>(context).state;
-                      // BlocProvider.of<ShopOrderBloc>(context).add(
-                      //   GetShopOrderEvent(
-                      //     userEmail: state.user!.email,
-                      //   ),
-                      // );
-                      controller.jumpToPage(0);
-                    },
-                  ),
-                  NavigationBarBottom(
-                    title: AppStrings.myWallet.tr(context),
-                    icon: IconAssets.wallet,
-                    onPress: () => controller.jumpToPage(1),
-                  ),
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  NavigationBarBottom(
-                    title: AppStrings.customerService.tr(context),
-                    icon: IconAssets.customersService,
-                    onPress: () => controller.jumpToPage(3),
-                  ),
-                  NavigationBarBottom(
-                    title: AppStrings.profile.tr(context),
-                    icon: IconAssets.profile,
-                    onPress: () => controller.jumpToPage(4),
-                  ),
-                ],
-              )
+    return Stack(
+      children: [
+        Scaffold(
+          body: PageView(
+            onPageChanged: (v) {
+              final authState = BlocProvider.of<AuthenticationBloc>(context).state;
+              if (v == 4 && authState is AuthenticationSuccess) {
+                BlocProvider.of<EmployeeBloc>(context)
+                    .add(GetEmployeeInfo(id: authState.employee.employeeID));
+              }
+            },
+            controller: controller,
+            children: const [
+              OrdersScreen(),
+              MyWalletScreen(),
+              MyOrdersScreen(),
+              CustomersServiceScreen(),
+              ProfileScreen(),
             ],
           ),
+          bottomNavigationBar: Container(
+            height: AppSize.s50.h,
+            color: ColorManager.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    NavigationBarBottom(
+                      title: AppStrings.orders.tr(context),
+                      icon: IconAssets.orders,
+                      onPress: () {
+                        controller.jumpToPage(0);
+                      },
+                    ),
+                    NavigationBarBottom(
+                      title: AppStrings.myWallet.tr(context),
+                      icon: IconAssets.wallet,
+                      onPress: () => controller.jumpToPage(1),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    NavigationBarBottom(
+                      title: AppStrings.customerService.tr(context),
+                      icon: IconAssets.customersService,
+                      onPress: () => controller.jumpToPage(3),
+                    ),
+                    NavigationBarBottom(
+                      title: AppStrings.profile.tr(context),
+                      icon: IconAssets.profile,
+                      onPress: () => controller.jumpToPage(4),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 20,
+          child: Center(
+            child: GestureDetector(
+              onTap: () {
+                controller.jumpToPage(2);
+              },
+              child: Image.asset(
+                IconAssets.home,
+                filterQuality: FilterQuality.high,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
